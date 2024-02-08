4cf85525d7bf8988632859fc48bf946241a57eb9
Nitish Nashine <nitishnashine@gmail.com>
2024-02-08 18:16:20 +0530

    working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { App } from "./todo/app";
+// import "todomvc-app-css/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+render(
+  <HashRouter>
+    <Routes>
+      <Route path="*" element={<App />} />
+    </Routes>
+  </HashRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
